e0484aa7bb31a8b61cde60a49e0ce1e63f91e9b0
lohage <desmond999.2000@mail.ru>
2019-04-04 00:53:55 +0700

    write geometry.h

---
+++ geometry.h
@@ -0,0 +1,14 @@
+#ifndef	GEOMETRY_H
+#define GEOMETRY_H
+
+typedef struct {
+	double value[10];
+	int type;
+	double S, P;
+} figure;
+
+int input_figure(char* arr, figure* fig);
+void SP_Triangle(figure* fig);
+void SP_Circle(figure* fig);
+
+#endif
