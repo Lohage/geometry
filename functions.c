f35c0b4b07aca3d55365b2cc70aff2815f1ee9ef
lohage <desmond999.2000@mail.ru>
2019-04-04 00:53:30 +0700

    write functions.c

---
+++ functions.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+#include "geometry.h"
+#define SQR(x) (x) * (x)
+#define PI 3.1416
+
+int input_figure(char* arr, figure* fig)
+{	
+	char* end;
+	char buf[100];
+    int i = 0;
+    while (arr[i] >= 'a' && arr[i] <= 'z') {
+		buf[i] = arr[i];
+		i++;
+	}
+	buf[i] = '\0';
+	if(!(strcmp(buf, "triangle"))){
+		fig->type = 1;
+	}
+	else if(!(strcmp(buf, "circle"))) {
+		fig->type = 2;
+	}				
+	else {
+		printf("error\n");
+		return 1;
+	}
+	end = arr;
+	i = 0;
+	while (*arr) {
+		while (!(isdigit(*arr) || *arr == '-') && *arr) {
+ 	 		arr++;
+    	}
+    	fig->value[i] = strtod(arr, &end);
+    	arr = end;
+    	i++; 
+	}
+	return 0;
+}
+
+void SP_Triangle(figure* fig)
+{
+	double a, b, c, p;
+	a = sqrt(SQR(fig->value[2] - fig->value[0]) + SQR(fig->value[3] - fig->value[1]));
+	b = sqrt(SQR(fig->value[4] - fig->value[2]) + SQR(fig->value[5] - fig->value[3]));
+	c = sqrt(SQR(fig->value[0] - fig->value[4]) + SQR(fig->value[1] - fig->value[5]));
+	fig->P = a + b + c;
+	p = fig->P / 2;
+	fig->S = sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+void SP_Circle(figure* fig)
+{
+	fig->P = 2 * PI * fig->value[2];
+	fig->S = PI * SQR(fig->value[2]);
+}
